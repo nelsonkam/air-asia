5d14cb8c9881a2a6af1b015051034eb3df1f161d
nelsonkam <kamganelson@gmail.com>
2017-03-13 18:23:01 +0100

    Init git repo.

---
+++ app/src/main/java/io/airasia/MainActivity.java
@@ -0,0 +1,22 @@
+package io.airasia;
+
+import android.support.design.widget.TabLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+    }
+}
