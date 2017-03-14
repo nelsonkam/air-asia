185508a4c7f999944b71a329e3ce5e0076f47e3d
nelsonkam <kamganelson@gmail.com>
2017-03-14 13:20:51 +0100

    Date Pickers fully functional

--- app/src/main/java/io/airasia/FlightFragment.java
+++ app/src/main/java/io/airasia/FlightFragment.java
@@ -1,17 +1,28 @@
 package io.airasia;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
-
-public class FlightFragment extends Fragment {
-
+public class FlightFragment extends Fragment implements View.OnClickListener, DatePickerDialog.OnDateSetListener, View.OnTouchListener {
+    EditText departureEditText;
+    EditText arrivalEditText;
+    DatePickerDialog datepicker;
+    EditText willReceiveDate;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -19,4 +30,43 @@ public class FlightFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_flight, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Calendar now = Calendar.getInstance();
+        datepicker = new DatePickerDialog(getContext(), this,
+                now.get(Calendar.YEAR), now.get(Calendar.MONTH), now.get(Calendar.DAY_OF_MONTH));
+        departureEditText = (EditText) view.findViewById(R.id.departure_edit_text);
+        departureEditText.setOnTouchListener(this);
+        arrivalEditText = (EditText) view.findViewById(R.id.arrival_edit_text);
+        arrivalEditText.setOnTouchListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+    }
+
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int dayOfMonth) {
+        Calendar date = Calendar.getInstance();
+        date.set(year, month, dayOfMonth);
+        String format = SimpleDateFormat.getDateInstance().format(date.getTime());
+        willReceiveDate.setText(format);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        int id = view.getId();
+        datepicker.show();
+        if (id == R.id.departure_edit_text) {
+            willReceiveDate = departureEditText;
+            return true;
+        } else if (id == R.id.arrival_edit_text) {
+            willReceiveDate = arrivalEditText;
+            return true;
+        }
+        return false;
+    }
 }
--- app/src/main/java/io/airasia/MainActivity.java
+++ app/src/main/java/io/airasia/MainActivity.java
@@ -1,17 +1,28 @@
 package io.airasia;
 
+import android.app.DatePickerDialog;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
 import android.net.Uri;
 import android.support.design.widget.TabLayout;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
     TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,17 +32,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
         setupViewPager(viewPager);
         tabLayout.setupWithViewPager(viewPager);
+
     }
+
     private void setupViewPager(ViewPager viewPager) {
         TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
         adapter.addFrag(new FlightFragment(), "Flight");
         adapter.addFrag(new FlightFragment(), "Train");
         adapter.addFrag(new FlightFragment(), "Bus");
         viewPager.setAdapter(adapter);
-    }
-
-    @Override
-    public void onClick(View view) {
     }
 
 
