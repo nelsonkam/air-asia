e65c4b435b35b783e512518f83e6ba29d33c4c8a
nelsonkam <kamganelson@gmail.com>
2017-03-14 08:57:18 +0100

    First day of work

--- app/src/main/java/io/airasia/MainActivity.java
+++ app/src/main/java/io/airasia/MainActivity.java
@@ -1,6 +1,8 @@
 package io.airasia;
 
+import android.net.Uri;
 import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -8,15 +10,29 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    Toolbar toolbar;
+    TabLayout tabLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        setupViewPager(viewPager);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+    private void setupViewPager(ViewPager viewPager) {
+        TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new FlightFragment(), "Flight");
+        adapter.addFrag(new FlightFragment(), "Train");
+        adapter.addFrag(new FlightFragment(), "Bus");
+        viewPager.setAdapter(adapter);
     }
 
     @Override
     public void onClick(View view) {
     }
+
+
 }
---
+++ app/src/main/java/io/airasia/TabPagerAdapter.java
@@ -0,0 +1,35 @@
+package io.airasia;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by nelson on 3/13/17.
+ */
+
+class TabPagerAdapter extends FragmentPagerAdapter {
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitleList = new ArrayList<>();
+    public TabPagerAdapter(FragmentManager manager) {
+        super(manager);
+    }
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.get(position);
+    }
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitleList.get(position);
+    }
+}
